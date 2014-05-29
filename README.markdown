61f90cd0041e3664cb0673b32432a8608380828f
Casey West <casey.west@whitehatsec.com>
2014-05-29 16:03:00 -0400

    Add base challenge files.

---
+++ README.markdown
@@ -0,0 +1,6 @@
+## WhiteHat Programming Challenges for Job Candidates
+
+`appliance-api` is a Ruby on Rails project designed to test a candidate's ability to write Ruby code for web applications at all levels.
+
+
+git am --directory=appliance-api < '/Users/caseywest/Library/Containers/it.bloop.airmail/Data/Library/Application Support/Airmail/General/Tmp/mario-submission/'*
--- appliance-api/db/seeds.rb
+++ appliance-api/db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+APPLIANCES = 1000
+MAX_TARGETS_PER_APPLIANCE = 5
+
+def randomize(string)
+  "#{rand 100000}-#{string}"
+end
+
+def create_appliance
+  appliance = Appliance.create! name:     randomize(Faker::Internet.domain_name),
+                                customer: Faker::Company.name
+
+  rand(MAX_TARGETS_PER_APPLIANCE + 1).times {create_target(appliance)}
+
+  if rand(100) > 90
+    Target.create! appliance: appliance,
+                   hostname:  randomize('localhost'),
+                   address:   '127.0.0.1'
+  end
+end
+
+def create_target(appliance)
+  Target.create! appliance: appliance,
+                 hostname:  randomize(Faker::Internet.domain_name),
+                 address:   Faker::Internet.ip_v4_address
+end
+
+APPLIANCES.times {create_appliance}
