21ae5493873fbff47b08cd02f0f730972f805420
Jessica Nebgen <jessica.nebgen@gmail.com>
2015-11-11 22:26:41 -0500

    Use count instead of select...count

--- appliance-api/app/models/report.rb
+++ appliance-api/app/models/report.rb
@@ -1,13 +1,13 @@
 class Report
 
-  attr_accessor :customer_count, :sad_customer_count, 
+  attr_accessor :customer_count, :sad_customer_count,
                 :unreachable_count, :target_count, :targets
 
   def initialize(appliances)
     @targets = appliances.map{ |a| a.targets.to_a }.flatten
     @customer_count = appliances.count
-    @sad_customer_count = appliances.select{|a| a.targets.any?{|t| !t.reachable? }}.count
-    @unreachable_count = @targets.select{|t| !t.reachable?}.count
+    @sad_customer_count = appliances.count {|a| a.targets.any?{|t| !t.reachable? }}
+    @unreachable_count = @targets.count {|t| !t.reachable?}
     @target_count = @targets.count
   end
 
